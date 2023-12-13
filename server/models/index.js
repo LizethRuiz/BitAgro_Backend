afd3a9f2f638a93cd71dbe22a169501879ebf81d
LizethRuiz <dianalizeth@hackademy.mx>
2023-12-12 21:38:06 -0700

    fixin models

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,16 @@
 const db = require('../config/database');
+const DataTypes = require('sequelize').DataTypes;
 
 const models = {
-  User: require('./users'),
-  Human_Resources: require('./human_resources'),
-  Lots: require('./lots'),
-  Cycles: require('./cycles'),
-  Sowing: require('./sowing'),
-  Finances: require('./finances'),
-  Harvest: require('./harvest'),
-  Binnacle: require('./binnacle'),
-  Activities: require('./activities'),
+  User: require('./users')(db, DataTypes),
+  Human_Resources: require('./human_resources')(db, DataTypes),
+  Lots: require('./lots')(db, DataTypes),
+  Cycles: require('./cycles')(db, DataTypes),
+  Sowing: require('./sowing')(db, DataTypes),
+  Finances: require('./finances')(db, DataTypes),
+  Harvest: require('./harvest')(db, DataTypes),
+  Binnacle: require('./binnacle')(db, DataTypes),
+  Activities: require('./activities')(db, DataTypes),
   db: db
 };
 
